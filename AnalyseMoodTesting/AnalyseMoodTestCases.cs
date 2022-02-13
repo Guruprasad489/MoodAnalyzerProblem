4210e39b969c90ee342ed9f37c735d9942b03383
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-13 21:43:27 +0530

    [Guru] Added exception handling in case user provides invalid mood, Test case -UC2

--- AnalyseMoodTesting/AnalyseMoodTestCases.cs
+++ AnalyseMoodTesting/AnalyseMoodTestCases.cs
@@ -7,7 +7,8 @@ namespace AnalyseMoodTesting
     [TestClass]
     public class AnalyseMoodTestCases
     {
-        [TestMethod]
+        //Method to test Sad Mood
+        [TestMethod]       
         [TestCategory ("Sad Message")]
         public void TestSadMoodInMessage()
         {
@@ -23,12 +24,30 @@ namespace AnalyseMoodTesting
             Assert.AreEqual(expected, actual);
         }
 
+        //Method to test Happy Mood
         [TestMethod]
         [TestCategory("Happy Message")]
         public void TestHappyMoodInMessage()
         {
             //Arrange
             string message = "I am in Any Mood";
+            string expected = "HAPPY";
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
+
+            //Act
+            string actual = moodAnalyzer.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Method to test Happy Mood in null message
+        [TestMethod]
+        [TestCategory("Null Exception")]
+        public void GivenNullMessageReturnHappyMood()
+        {
+            //Arrange
+            string message = null;
             string expected = "HAPPY";
             MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
 
