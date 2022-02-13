f39a782dd82a999d245cc6b237998e4e0aa1b0b3
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-13 19:03:00 +0530

    [Guru] Welcome to Mood Analyzer program

---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Anayzer Program");
+
+            Console.ReadLine();
+        }
+    }
+}
