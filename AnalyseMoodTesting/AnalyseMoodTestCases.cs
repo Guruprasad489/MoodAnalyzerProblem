5afa6b14cef8b08ca89bd1b6740d9fc404a56408
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-16 00:02:36 +0530

    [Guru] Using reflector to create Mood Analyzer with parameter constructor, Test cases -UC5

--- AnalyseMoodTesting/AnalyseMoodTestCases.cs
+++ AnalyseMoodTesting/AnalyseMoodTestCases.cs
@@ -8,6 +8,13 @@ namespace AnalyseMoodTesting
     [TestClass]
     public class AnalyseMoodTestCases
     {
+        MoodAnalyserFactory factory;
+        [TestInitialize]
+        public void Setup()
+        {
+            factory = new MoodAnalyserFactory();
+        }
+
         //TC 1.1 - Method to test Sad Mood
         [TestMethod]       
         [TestCategory ("Sad Message")]
@@ -91,9 +98,8 @@ namespace AnalyseMoodTesting
             MoodAnalyzer expected = new MoodAnalyzer();
             object obj = null;
 
-                MoodAnalyserFactory factory = new MoodAnalyserFactory();
-                obj = factory.CreateMoodMoodAnalyse(className, constructorName);
-                expected.Equals(obj);
+            obj = factory.CreateMoodMoodAnalyse(className, constructorName);
+            expected.Equals(obj);
         }
 
         //TC 4.2 - improper class details are provided and expected to throw exception Class not found
@@ -104,7 +110,6 @@ namespace AnalyseMoodTesting
         {
             try
             {
-                MoodAnalyserFactory factory = new MoodAnalyserFactory();
                 object actual = factory.CreateMoodMoodAnalyse(className, constructorName);
             }
             catch (MoodAnalyzerException ex)
@@ -121,12 +126,72 @@ namespace AnalyseMoodTesting
         {
             try
             {
-                MoodAnalyserFactory factory = new MoodAnalyserFactory();
                 object actual = factory.CreateMoodMoodAnalyse(className, constructorName);
             }
             catch (MoodAnalyzerException ex)
             {
                 Assert.AreEqual(expected, ex.Message);
+            }
+        }
+
+        //TC 5.1 - Method to test moodanalyser class with parameter constructor to check if two objects are equal
+        [TestCategory("Reflection")]
+        [TestMethod]
+        [DataRow("I am in Happy mood")]
+        [DataRow("I am in Sad mood")]
+        [DataRow("I am in any mood")]
+        public void GivenMessageReturnParameterizedConstructor(string message)
+        {
+            MoodAnalyzer expected = new MoodAnalyzer(message);
+            object obj = null;
+            try
+            {
+                obj = factory.CreateMoodMoodAnalyserParameterObject("MoodAnalyzer", "MoodAnalyzer", message);
+            }
+            catch (MoodAnalyzerException actual)
+            {
+                Assert.AreEqual(expected, actual.Message);
+            }
+            obj.Equals(expected);
+        }
+
+        //TC 5.2 - Method to test moodanalyser with diff class with parameter constructor to throw error
+        [TestCategory("Reflection")]
+        [TestMethod]
+        [DataRow("Company", "I am in Happy mood", "Could not find class")]
+        [DataRow("Student", "I am in Sad mood", "Could not find class")]
+        public void GivenMessageReturnParameterizedClassNotFound(string className, string message, string expextedError)
+        {
+            MoodAnalyzer expected = new MoodAnalyzer(message);
+            object obj = null;
+            try
+            {
+                obj = factory.CreateMoodMoodAnalyserParameterObject(className, "MoodAnalyzer", message);
+
+            }
+            catch (MoodAnalyzerException actual)
+            {
+                Assert.AreEqual(expextedError, actual.Message);
+            }
+        }
+
+        //TC 5.3 - Method to test moodanalyser with diff constructor with parameter constructor to throw error
+        [TestCategory("Reflection")]
+        [TestMethod]
+        [DataRow("Customer", "I am in Happy mood", "Could not find constructor")]
+        [DataRow("Student", "I am in Sad mood", "Could not find constructor")]
+        public void GivenMessageReturnParameterizedConstructorNotFound(string constructor, string message, string expextedError)
+        {
+            MoodAnalyzer expected = new MoodAnalyzer(message);
+            object obj = null;
+            try
+            {
+                obj = factory.CreateMoodMoodAnalyserParameterObject("MoodAnalyzer", constructor, message);
+
+            }
+            catch (MoodAnalyzerException actual)
+            {
+                Assert.AreEqual(expextedError, actual.Message);
             }
         }
     }
