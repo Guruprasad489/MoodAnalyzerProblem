10a8d6452b87c79ef8f6d1cb6919f296f93088d8
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-16 17:40:12 +0530

    [Guru] Implemented annotation concept

--- MoodAnalyzerProblem/MoodAnalyzer.cs
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace MoodAnalyzerProblem
 {
     public class MoodAnalyzer
     {
+        [Required(ErrorMessage ="{0} Should not be null or empty")]        //Annotation - Attribute
         public string message;
 
         public MoodAnalyzer()
--- MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,28 @@ namespace MoodAnalyzerProblem
             Console.WriteLine("Welcome to Mood Anayzer Program");
 
             //UC1 - Creating MoodAnalyzer object
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("");
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Happy");
             Console.WriteLine(moodAnalyzer.AnalyseMood());
+            ValidateMoodAnalyzer(moodAnalyzer);
             Console.ReadLine();
+        }
+        //Method to Vaidate MoodAnalyzer Property Using Annotations
+        public static void ValidateMoodAnalyzer(MoodAnalyzer moodAnalyzer)
+        {
+            ValidationContext validationContext = new ValidationContext(moodAnalyzer, null, null);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(moodAnalyzer, validationContext, validationResults, true);
+            if (!valid)
+            {
+                foreach (ValidationResult validationResult in validationResults)
+                {
+                    Console.WriteLine(validationResult.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("All Validations are successful");
+            }
         }
     }
 }
